e9c41015b7c2d6f8d676a46b4ffed41e7caead0d
liusitong <liust@huabo.tech>
2022-02-23 18:36:43 +0800

    重构界面

--- src/popup/App/App.vue
+++ src/popup/App/App.vue
@@ -18,15 +18,29 @@
       </el-header>
       <el-main>
         <el-row>
-          <el-descriptions title="好友数据操作" :column="2">
+          <el-descriptions title="好友数据操作" :column="2" direction="vertical">
             <el-descriptions-item><el-button size="small" @click="getAllUserInfo()">获取所有用户</el-button></el-descriptions-item>
             <el-descriptions-item><el-button size="small" @click="saveUserToExcel()">保存用户信息为excel表</el-button></el-descriptions-item>
-            <el-descriptions-item><el-button size="small">读取excel好友表</el-button></el-descriptions-item>
+            <el-descriptions-item>
+              <el-upload
+                  class="upload-demo"
+                  multiple
+                  :limit="1"
+                  :auto-upload="false"
+                  :on-change="handleUserExcel"
+                  :file-list="fileList"
+                  accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet">
+                <el-button size="small">读取excel好友表</el-button>
+                <template #tip>
+                  <div class="el-upload__tip">只能上传 1 个 excel 文件, 只支持 xlsx 格式</div>
+                </template>
+              </el-upload>
+            </el-descriptions-item>
           </el-descriptions>
         </el-row>
         <el-divider></el-divider>
         <el-row>
-          <el-descriptions title="自动化信息设置" :column="2">
+          <el-descriptions title="自动化信息设置" :column="2" direction="vertical">
             <el-descriptions-item><el-button size="small">消息文本</el-button></el-descriptions-item>
             <el-descriptions-item><el-button size="small">定时设置</el-button></el-descriptions-item>
           </el-descriptions>
@@ -48,7 +62,7 @@
         </el-row>
       </el-header>
       <el-main>
-        <el-collapse v-model="activeNames" @change="handleChange">
+        <el-collapse>
           <el-collapse-item title="全部聊天" name="1">
             <el-table
                 ref="filterHandler"
@@ -61,19 +75,19 @@
               <el-table-column type="selection" width="20"/>
               <el-table-column property="image_url" label="头像" width="50">
                 <template #default="scope">
-                  <el-avatar :size="50" :src="scope.row.image_url"></el-avatar>
+                  <el-avatar :size="42" :src="scope.row.image_url"></el-avatar>
                 </template>
               </el-table-column>
 <!--              <el-table-column property="uid" label="uid"/>-->
               <el-table-column property="uname" label="用户名"/>
               <el-table-column property="area" label="区域"  width="40"
-                               :filters="[
-                                { text: '未分区', value: '-' },
-                                { text: '美国', value: '美国' },
-                              ]"
+                               :filters="areaFilters"
                                :filter-method="filterArea"
                                filter-placement="bottom-end"/>
             </el-table>
+          </el-collapse-item>
+          <el-collapse-item title="任务面板" name="2">
+
           </el-collapse-item>
         </el-collapse>
       </el-main>
@@ -84,10 +98,10 @@
 <script>
 import { ElContainer, ElHeader, ElMain, ElFooter, ElImage, ElRow, ElCol, ElButton,
   ElDivider, ElDescriptions, ElDescriptionsItem, ElIcon, ElCollapse, ElCollapseItem,
-  ElTable, ElTableColumn, ElAvatar} from 'element-plus';
+  ElTable, ElTableColumn, ElAvatar, ElUpload, ElMessage} from 'element-plus';
 import { Close } from '@element-plus/icons-vue';
 import 'element-plus/dist/index.css';
-import { utils, writeFile } from "xlsx";
+import { utils, writeFile, read } from "xlsx";
 const handleSelectionChange = (val) => {
   multipleSelection.value = val
 }
@@ -97,13 +111,17 @@ export default {
     ElContainer, ElHeader, ElMain, ElFooter, ElImage,
     ElRow, ElCol, ElCollapse, ElCollapseItem,
     ElButton, ElDivider, ElDescriptions, ElDescriptionsItem, ElIcon,
-    Close, ElTable, ElTableColumn, ElAvatar
+    Close, ElTable, ElTableColumn, ElAvatar, ElUpload
   },
   data() {
     return {
       actionPanelIsShow: false,
       allUserInfo:[],
       userSelection: [],
+      fileList: [],
+      areaFilters: [
+        { text: '未分区', value: '-' },
+      ]
     }
   },
   methods: {
@@ -112,6 +130,30 @@ export default {
     },
     handleSelectionChange(val) {
       this.userSelection = val
+    },
+    handleUserExcel(file, fileList) {
+      let reader = new FileReader();
+      reader.onload = function(e) {
+        let data = e.target.result;
+        let workbook = read(data, {type: 'binary'});
+        let first_sheet_name = workbook.SheetNames[0];
+
+        /* Get worksheet */
+        let worksheet = workbook.Sheets[first_sheet_name];
+
+        let all_user = {}
+        let range = utils.decode_range(worksheet["!ref"])
+        for (let i = range.s.r + 1; i < range.e.r; i++) {
+          all_user[worksheet[utils.encode_col(1) + utils.encode_row(i)].v] = {
+              image_url:worksheet[utils.encode_col(0) + utils.encode_row(i)].v,
+              uid: worksheet[utils.encode_col(1) + utils.encode_row(i)].v,
+              uname: worksheet[utils.encode_col(2) + utils.encode_row(i)].v,
+              area: worksheet[utils.encode_col(3) + utils.encode_row(i)].v
+          }
+        }
+        localStorage.setItem("excel_user", JSON.stringify(all_user));
+      }
+      reader.readAsBinaryString(file.raw);
     },
     showDataPanel() {
       let main_app = document.getElementsByClassName("main_app")[0];
@@ -138,12 +180,28 @@ export default {
 
     getAllUserInfo(){
       let this_ = this
+      let excelAllUser = null
+      let excelAllUserStr = localStorage.getItem("excel_user");
+      if (excelAllUserStr){
+        excelAllUser = JSON.parse(excelAllUserStr);
+      }
       chrome.runtime.sendMessage({type:'getUserInfo'},function(response) {
         console.log(response.data)
         let data = [];
+        let allFilter = new Set();
+        this_.areaFilters = [{ text: '未分区', value: '-' }]
         Object.keys(response.data).forEach(function (key){
-          data.push(response.data[key]);
+          let new_user = response.data[key];
+          if (excelAllUser != null && excelAllUser[key] != undefined) {
+            new_user['area'] = excelAllUser[key].area;
+            allFilter.add(excelAllUser[key].area);
+          }
+          data.push(new_user);
+
         })
+        for(let key of allFilter){
+          this_.areaFilters.push({ text: key, value: key })
+        }
         this_.allUserInfo = data;
       });
       this.showDataPanel();
@@ -230,5 +288,8 @@ body{
 /*鼠标按下，没有抬起*/
 #closeIcon:active {
   color: #ffff;
+}
+.el-message{
+  min-width: 260px !important;
 }
 </style>
